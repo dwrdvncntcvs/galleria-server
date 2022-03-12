5db9b4fbd2a5488a44e2a9140d96a43b26367390
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-12 17:00:53 +0800

    User route and controller added

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,2 +1,24 @@
 const { sequelize, User } = require("../../models");
 
+exports.createNewUser = async (req, res) => {
+  const { first_name, last_name, username, email, password } = req.body;
+
+  const t = await sequelize.transaction();
+  try {
+    await User.create(
+      { first_name, last_name, username, email, password },
+      { transaction: t }
+    );
+    await t.commit();
+
+    return res.status(200).send({ msg: "Account created.", user: req.body });
+  } catch (err) {
+    const errorMessage =
+      typeof err.message === "string"
+        ? err.message.split(": ")[1].split(",\n")
+        : err.message;  
+    await t.rollback();
+
+    return res.status(errorMessage[0]).send({ msg: errorMessage[1] });
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { createNewUser } = require("../controllers/userControllers");
 
 const routes = express.Router();
+
+routes.post("/sign-up", createNewUser);
 
 module.exports = routes;
