9823dedeaa3dd527e08e5eff1a08403a30703371
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-12 16:42:51 +0800

    User routes added on app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("../models");
+const { userRoutes } = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
