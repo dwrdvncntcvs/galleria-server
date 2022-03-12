93d7819936ef0b65166423564fb86a754af6b41e
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-12 17:15:26 +0800

    Middleware for cheking existence of username and email added to sign up route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const { createNewUser } = require("../controllers/userControllers");
+const {
+  checkIfEmailExists,
+  checkIfUsernameExist,
+} = require("../middlewares/userMiddleware");
 
 const routes = express.Router();
 
-routes.post("/sign-up", createNewUser);
+routes.post(
+  "/sign-up",
+  [checkIfUsernameExist, checkIfEmailExists],
+  createNewUser
+);
 
 module.exports = routes;
