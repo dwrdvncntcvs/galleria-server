926557979c727ca23d3fec4bd2fa33360b913746
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-13 14:05:51 +0800

    User profile route created with middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { createNewUser, signIn } = require("../controllers/userControllers");
+const {
+  createNewUser,
+  signIn,
+  userProfile,
+} = require("../controllers/userControllers");
 const {
   checkIfEmailExists,
   validatePassword,
+  authenticate,
+  checkIfUsernameExist,
 } = require("../middlewares/userMiddleware");
 
 const routes = express.Router();
@@ -10,5 +16,11 @@ const routes = express.Router();
 routes.post("/sign-up", createNewUser);
 
 routes.post("/sign-in", [checkIfEmailExists, validatePassword], signIn);
+
+routes.get(
+  "/profile/:username",
+  [authenticate, checkIfUsernameExist],
+  userProfile
+);
 
 module.exports = routes;
