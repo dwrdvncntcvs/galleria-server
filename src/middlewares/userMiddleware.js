fcd14c791394a38f18ba66b9a769362e8a13550b
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-13 13:41:31 +0800

    Middleware for validating password

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -4,7 +4,21 @@ exports.checkIfEmailExists = async (req, res, next) => {
   const { email } = req.body;
 
   const user = await User.findUserByEmail(email);
-  if (!user) return res.status(403).send({ msg: "Email doesn't exist." });
+  if (!user)
+    return res.status(403).send({ msg: "Email or password incorrect." });
+
+  req.currentUser = user;
+  next();
+};
+
+exports.validatePassword = async (req, res, next) => {
+  const { password } = req.body;
+
+  const user = req.currentUser;
+  const isValid = await User.comparePassword(password, user.password);
+
+  if (!isValid)
+    return res.status(403).send({ msg: "Email or password incorrect." });
 
   next();
 };
