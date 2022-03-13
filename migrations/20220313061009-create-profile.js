649ae47eccd00a07a89f4c120ea0bbe6731c3dac
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-13 14:10:38 +0800

    Profile model and migration created

---
+++ migrations/20220313061009-create-profile.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      bio: {
+        type: Sequelize.TEXT
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
---
+++ models/profile.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Profile.init({
+    bio: DataTypes.TEXT,
+    gender: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Profile',
+  });
+  return Profile;
+};
