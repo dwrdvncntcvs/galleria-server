2b6d99a19b549577f233ec364a11d97f8b811b98
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-13 14:16:25 +0800

    Relationship between profile and user model created

--- models/user.js
+++ models/user.js
@@ -8,8 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Profile }) {
+      this.hasOne(Profile, { foreignKey: "userId" });
     }
   }
   User.init(
