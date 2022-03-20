c2fb2a5841eae1ac5f6faa8d86e0990df84944b9
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-20 17:15:43 +0800

    Avatar Route is beging used on main app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("../models");
-const { userRoutes } = require("./routes");
+const { userRoutes, avatarRoutes } = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(userRoutes);
+app.use(avatarRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
