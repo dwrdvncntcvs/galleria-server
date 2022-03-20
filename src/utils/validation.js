1b750f6ea77337cd5e97006f7cd775bb057d0794
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-20 17:10:38 +0800

    Validation added

---
+++ src/utils/validation.js
@@ -0,0 +1,6 @@
+exports.isUuidValid = (uuid) => {
+  const pattern =
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+  return pattern.test(uuid) ? true : false;
+};
