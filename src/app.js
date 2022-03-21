284721240750c311d9fa78d2c1d2aea7e2f4cd6a
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-22 07:56:57 +0800

    Adding static directory images/avatar

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/images/avatars", express.static("images/avatars"));
+
 app.use(userRoutes);
 app.use(avatarRoutes);
 
