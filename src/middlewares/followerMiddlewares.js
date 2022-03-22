bf13b3b25a66a7b2d4b31bff41453eeb754e9386
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-22 21:30:23 +0800

    Middleware for checking if user can follow other user

---
+++ src/middlewares/followerMiddlewares.js
@@ -0,0 +1,16 @@
+const { User, Follower } = require("../../models");
+
+exports.canFollow = async (req, res, next) => {
+  const username = req.params.username;
+  const user = req.user;
+  const foundUser = await User.findUserByUsername(username);
+
+  const isUserFollowed = await Follower.isUserFollowed(foundUser, user);
+  if (isUserFollowed)
+    return res.status(403).send({
+      msg: `You have already followed ${foundUser.first_name} ${foundUser.last_name}`,
+    });
+
+  req.foundUser = foundUser;
+  next();
+};
