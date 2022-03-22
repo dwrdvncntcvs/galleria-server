8f725a83353860caf53d39ea7affd831cf260dfc
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-22 14:57:04 +0800

    Route for adding new avatar created with proper middlewares

--- src/routes/avatarRoutes.js
+++ src/routes/avatarRoutes.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const { uploadAvatar } = require("../controllers/avatarControllers");
+const { checkIfAvatarExist } = require("../middlewares/avatarMiddlewares");
+const { authenticate } = require("../middlewares/userMiddleware");
+const Image = require("../utils/images");
 
 const routes = express.Router();
+
+const upload = new Image({
+  path: "images/avatars",
+  name: "avatar",
+  uploadType: "single",
+}).upload();
+
+routes.post(
+  "/add-avatar/:userId",
+  [authenticate, upload, checkIfAvatarExist],
+  uploadAvatar
+);
 
 module.exports = routes;
