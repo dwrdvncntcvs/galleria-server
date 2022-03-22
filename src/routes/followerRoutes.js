cdd3a0133ceabc59edc78b3d889d7ef90563758d
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-22 21:31:22 +0800

    Route for following user created with its respective middlewares

--- src/routes/followerRoutes.js
+++ src/routes/followerRoutes.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const { followUser } = require("../controllers/followerController");
+const { canFollow } = require("../middlewares/followerMiddlewares");
+const {
+  authenticate,
+  checkIfUsernameExist,
+} = require("../middlewares/userMiddleware");
 
 const routes = express.Router();
+
+routes.post(
+  "/follow/:username",
+  [authenticate, checkIfUsernameExist, canFollow],
+  followUser
+);
 
 module.exports = routes;
