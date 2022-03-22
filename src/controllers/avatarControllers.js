da9d2b54509ff3eb52629fe7e01641110ceb26ef
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-22 14:57:41 +0800

    Controller for saving image details into the database create

---
+++ src/controllers/avatarControllers.js
@@ -0,0 +1,23 @@
+const { sequelize, Avatar } = require("../../models");
+const { errorMessage } = require("../utils/error");
+
+exports.uploadAvatar = async (req, res) => {
+  const { filename, path, mimetype, size } = req.file;
+  const { id } = req.user;
+
+  const t = await sequelize.transaction();
+  try {
+    await Avatar.update(
+      { filename, path, mimetype, size },
+      { where: { userId: id } },
+      { transaction: t }
+    );
+    await t.commit();
+
+    return res.status(200).send({ msg: "Avatar Uploaded." });
+  } catch (err) {
+    const { status, msg } = errorMessage(err);
+    await t.rollback();
+    return res.status(status).send({ msg });
+  }
+};
