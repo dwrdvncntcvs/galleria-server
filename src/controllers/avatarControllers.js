b9ceba1c771c52b9853e2b04bb66808a68392566
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-22 17:29:50 +0800

    Controller for removing avatar created

--- src/controllers/avatarControllers.js
+++ src/controllers/avatarControllers.js
@@ -21,3 +21,23 @@ exports.uploadAvatar = async (req, res) => {
     return res.status(status).send({ msg });
   }
 };
+
+exports.removeAvatar = async (req, res) => {
+  const id = req.params.id;
+
+  const t = await sequelize.transaction();
+  try {
+    await Avatar.update(
+      { filename: "", path: "", mimetype: "", size: "" },
+      { where: { userId: id } },
+      { transaction: t }
+    );
+    await t.commit();
+
+    return res.status(200).send({ msg: "Avatar Deleted." });
+  } catch (err) {
+    const { status, msg } = errorMessage(err);
+    await t.rollback();
+    return res.status(status).send({ msg });
+  }
+};
