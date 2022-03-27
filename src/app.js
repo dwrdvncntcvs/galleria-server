7c3620b9bc8f3e39b84d03cfc9a6aaa43413a19b
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-27 19:06:08 +0800

    Post routes is being used on app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("../models");
-const { userRoutes, avatarRoutes, followerRoutes } = require("./routes");
+const {
+  userRoutes,
+  avatarRoutes,
+  followerRoutes,
+  postRoutes,
+} = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,6 +19,7 @@ app.use("/images/avatars", express.static("images/avatars"));
 app.use(userRoutes);
 app.use(avatarRoutes);
 app.use(followerRoutes);
+app.use(postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
