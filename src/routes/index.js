2bf5661f811973e484efd08ff023eb8fcef93d81
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-27 19:06:42 +0800

    Post routes is being exported

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
 exports.userRoutes = require("./userRoutes");
 exports.avatarRoutes = require("./avatarRoutes");
 exports.followerRoutes = require("./followerRoutes");
+exports.postRoutes = require("./postRoutes");
