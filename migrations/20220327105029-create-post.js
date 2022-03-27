3eb143e14a17c778095c58481fddd79078c51e54
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-27 18:58:20 +0800

    Post Model and Migration Created

---
+++ migrations/20220327105029-create-post.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("posts", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      imageId: {
+        type: Sequelize.UUID,
+        allowNull: true,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("posts");
+  },
+};
---
+++ models/post.js
@@ -0,0 +1,45 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Post.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: [403, "\nPost should not be empty."] },
+          notNull: { msg: [403, "\nPost should not be empty."] },
+        },
+      },
+      imageId: {
+        type: DataTypes.UUID,
+        allowNull: true,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Post",
+      tableName: "posts",
+    }
+  );
+  return Post;
+};
