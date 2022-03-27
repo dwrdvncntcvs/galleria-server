e670451e32c762f7e806488f22d6146eea3a46da
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-27 20:22:58 +0800

    Route for creating new text post added

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const { createTextPost } = require("../controllers/postController");
+const { authenticate } = require("../middlewares/userMiddleware");
 
 const routes = express.Router();
+
+routes.post("/create-new-post/text", [authenticate], createTextPost);
 
 module.exports = routes;
