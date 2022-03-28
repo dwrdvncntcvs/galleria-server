470c06d1e2ba51cc8b6f3a782d9531ef1fc155f3
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-28 08:09:44 +0800

    Creating image post added and some middlewares added

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,9 +1,33 @@
 const express = require("express");
-const { createTextPost } = require("../controllers/postController");
+const {
+  createTextPost,
+  createImagePost,
+} = require("../controllers/postController");
+const { hasText, isImageValid } = require("../middlewares/postMiddlewares");
 const { authenticate } = require("../middlewares/userMiddleware");
+const { TEXT } = require("../utils/constant");
+const Image = require("../utils/images");
+
+const upload = (type) => {
+  return new Image({
+    path: "images/posts",
+    name: "imagePost",
+    uploadType: type,
+  }).upload();
+};
 
 const routes = express.Router();
 
-routes.post("/create-new-post/text", [authenticate], createTextPost);
+routes.post(
+  "/create-new-post/text",
+  [authenticate, hasText(TEXT)],
+  createTextPost
+);
+
+routes.post(
+  "/create-new-post/image",
+  [authenticate, upload("single"), isImageValid("images/posts")],
+  createImagePost
+);
 
 module.exports = routes;
