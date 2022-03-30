0442c3e6649a3f7154d86781fd7d6e33b6059a00
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-03-30 08:34:59 +0800

    Controller for creating images post created

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,6 @@
 const { sequelize, Post, ImagePost } = require("../../models");
 const { errorMessage } = require("../utils/error");
+const { addKey } = require("../utils/helper");
 
 exports.createTextPost = async (req, res) => {
   const { content } = req.body;
@@ -38,6 +39,32 @@ exports.createImagePost = async (req, res) => {
 
     return res.status(200).send({ msg: "Posted." });
   } catch (err) {
+    const { status, msg } = errorMessage(err);
+    await t.rollback();
+    return res.status(status).send({ msg });
+  }
+};
+
+exports.createImagesPost = async (req, res) => {
+  const { content } = req.body;
+  const files = req.filesArr;
+  const user = req.user;
+
+  const t = await sequelize.transaction();
+  try {
+    const post = await Post.create(
+      { content, userId: user.id },
+      { transaction: t }
+    );
+
+    const filesArr = addKey(files, post.id, "postId");
+
+    await ImagePost.bulkCreate(filesArr, { transaction: t });
+    await t.commit();
+
+    return res.status(200).send({ msg: "Images Posted." });
+  } catch (err) {
+    console.log(err);
     const { status, msg } = errorMessage(err);
     await t.rollback();
     return res.status(status).send({ msg });
