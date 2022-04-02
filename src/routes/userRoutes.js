7459d9cf6600de685c981e4e5e9cc158989899ac
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-04-02 08:33:22 +0800

    Minor changes on routes. Params to query

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -22,12 +22,12 @@ routes.post("/sign-in", [checkIfEmailExists, validatePassword], signIn);
 
 routes.get(
   "/profile/:username",
-  [authenticate, checkIfUsernameExist],
+  [ checkIfUsernameExist],
   userProfile
 );
 
-routes.put("/update/profile/:id", [authenticate, canEdit], updateProfile);
+routes.put("/update/profile", [authenticate, canEdit], updateProfile);
 
-routes.delete("/delete/user/:id", [authenticate, canEdit], deleteUser);
+routes.delete("/delete/user", [authenticate, canEdit], deleteUser);
 
 module.exports = routes;
