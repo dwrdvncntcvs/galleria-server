7248a5f34275e8d080379ca7a36fccb2df1c7bc6
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-05-23 11:09:14 +0800

    Cookie parser is used on on app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const { sequelize } = require("../models");
 const {
   userRoutes,
@@ -12,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/images/avatars", express.static("images/avatars"));
