dc33d7749e6efc061ab7704e011cf30ed7780a7e
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-09-25 12:51:01 +0800

    Sequelize Config update

--- config/config.js
+++ config/config.js
@@ -16,10 +16,17 @@ module.exports = {
     dialect: process.env.DATABASE_DIALECT,
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT,
+    use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
 };
